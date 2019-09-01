7c4d7d524c73299fbb76f1dcd57b65ecb71d40f3
Hang Quoc Tai <52644127+hangquoctai@users.noreply.github.com>
2019-09-01 19:11:23 +0700

    Delete Router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import ProductsContainer from "./container/ProductsContainer.js";
 import MessageContainer from "./container/MessageContainer";
 import Footer from "./components/Footer.js";
 import CartContainer from "./container/CartContainer.js";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Login from "./components/login";
 
 class App extends Component {
   render() {
     return (
-      <Router>
         <div>
           <Header />
           <main id="mainContainer">
@@ -19,12 +16,9 @@ class App extends Component {
               <MessageContainer />
               <CartContainer />
             </div>
-            <Link exact to="/login">Đăng nhập</Link>
-            <Route path="/login" exact component={Login} />
           </main>
           <Footer />
         </div>
-      </Router>
     );
   }
 }
